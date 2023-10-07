2786a4c8583c89a5d5a1538c40742afe3185de12
Najibullah <najib2020202020@gmail.com>
2023-10-07 10:08:06 +0430

    Update linters

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
+# Rubocop:disable Metrics/BlockLength
 ActiveRecord::Schema[7.0].define(version: 20_231_005_230_605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -59,3 +59,4 @@ ActiveRecord::Schema[7.0].define(version: 20_231_005_230_605) do
   add_foreign_key 'likes', 'users'
   add_foreign_key 'posts', 'users'
 end
+#  Rubocop:enable Metrics/BlockLength
