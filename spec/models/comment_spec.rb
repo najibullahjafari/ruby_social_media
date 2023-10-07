b6ff9529e762d76ffe6622b089cdc4c61281d169
Najibullah <najib2020202020@gmail.com>
2023-10-07 14:34:57 +0430

    Fiz linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   user = User.new(name: 'najib', posts_counter: 0)
   post = Post.new(title: 'man', comments_counter: 0, likes_counter: 0)
-  it "is vlaid with a user and a post" do
-    cm = Comment.new(user: user, post: post)
+  it 'is vlaid with a user and a post' do
+    cm = Comment.new(user:, post:)
     expect(cm).to be_valid
   end
 
-  it "is invalid without user" do
-    cm = Comment.new(post: post)
+  it 'is invalid without user' do
+    cm = Comment.new(post:)
     expect(cm).to be_invalid
   end
 
-  it "the comment sould not be present" do
-    cm = Comment.new(user: user, post: post)
+  it 'the comment sould not be present' do
+    Comment.new(user:, post:)
     fountp = Post.find_by(id: post.id)
     expect(fountp).to_not be_present
   end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   user = User.new(name: 'najib', posts_counter: 0)
   post = Post.new(title: 'man', comments_counter: 0, likes_counter: 0)
-  it "is vlaid with a user and a post" do
-    like = Like.new(user: user, post: post)
+  it 'is vlaid with a user and a post' do
+    like = Like.new(user:, post:)
     expect(like).to be_valid
   end
 
-  it "is invalid without user" do
-    lik = Like.new(post: post)
+  it 'is invalid without user' do
+    lik = Like.new(post:)
     expect(lik).to be_invalid
   end
 
-  it "the like sould not be present" do
-    like = Like.new(user: user, post: post)
+  it 'the like sould not be present' do
+    Like.new(user:, post:)
     fountp = Post.find_by(id: post.id)
     expect(fountp).to_not be_present
   end
