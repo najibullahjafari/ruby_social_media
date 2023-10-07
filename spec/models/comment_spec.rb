cf64fdfcdd24975fee9e7b0c22e222a6b6da3e74
Najibullah <najib2020202020@gmail.com>
2023-10-07 17:21:21 +0430

    update

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -25,36 +25,4 @@ RSpec.describe Like, type: :model do
     expect(fountp).to_not be_present
   end
   
-  require 'rails_helper'
-
-RSpec.describe Like, type: :model do
-  user = User.new(name: 'najib', posts_counter: 0)
-  post = Post.new(title: 'man', comments_counter: 0, likes_counter: 0)
-  it 'is vlaid with a user and a post' do
-    cm = Comment.new(user:, post:)
-    expect(cm).to be_valid
-  end
-
-  it 'is invalid without user' do
-    cm = Comment.new(post:)
-    expect(cm).to be_invalid
-  end
-
-  it 'the comment sould not be present' do
-    Comment.new(user:, post:)
-    fountp = Post.find_by(id: post.id)
-    expect(fountp).to_not be_present
-  end
-  
-  it 'updates comments_counter when a comment is created' do
-    user = User.create(name: 'najib')
-    post = user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0)
-    expect(post.comments_counter).to eq(0)
-
-    comment = post.comments.create(user: user, text: 'Nice post!')
-    post.reload
-
-    expect(comment).to be_valid
-    expect(post.comments_counter).to eq(1)
-  end
 end
