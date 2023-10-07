20fe1e100755aa1e52fc4d76facca401ec3883a5
Najibullah <najib2020202020@gmail.com>
2023-10-07 10:33:21 +0430

    Update migrations

--- db/migrate/20231005110207_create_posts.rb
+++ db/migrate/20231005110207_create_posts.rb
@@ -3,9 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.string :comments_counter
-      t.string :likes_counter
-      # t.integer :author_id
+      t.string :comments_counter 
+      t.string :likes_counter 
+      t.integer :author_id
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/migrate/20231005110248_create_likes.rb
+++ db/migrate/20231005110248_create_likes.rb
@@ -1,8 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      # t.integer :user_id
-      # t.integer :post_id
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
 
