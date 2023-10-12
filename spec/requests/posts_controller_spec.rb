211a10a876dfccc0474bb4651a67061102ff02dc
najibullah <najib2020202020@gmail.com>
2023-10-12 10:08:51 +0430

    Update tests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PostsController, type: :controller do
   let(:user) { User.create!(name: 'Test User', bio: 'Test bio.') }
 
   describe 'GET #index' do
-    it 'assigns @user and @posts' do
+    it 'If the response body includes correct placeholder text.' do
       user.posts.create(title: 'Post 1', text: 'This is post 1')
       user.posts.create(title: 'Post 2', text: 'This is post 2')
 
@@ -12,20 +12,35 @@ RSpec.describe PostsController, type: :controller do
       expect(assigns(:user)).to eq(user)
       expect(assigns(:posts)).not_to be_empty
     end
+
+    it 'returns a successful response' do
+      get :index, params: { user_id: user.id }
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get :index, params: { user_id: user.id }
+      expect(response).to render_template(:index)
+    end
   end
 
   describe 'GET #show' do
     let(:post) { Post.create!(title: 'Test Post', text: 'This is a test post.', author: user) }
 
-    it 'assigns @user and @post' do
+    it 'If the response body includes correct placeholder text.' do
       get :show, params: { user_id: user.id, id: post.id }
       expect(assigns(:user)).to eq(user)
       expect(assigns(:post)).to eq(post)
     end
 
-    it 'returns a successful response' do
+    it 'If response status was correct.' do
       get :show, params: { user_id: user.id, id: post.id }
       expect(response).to have_http_status(200)
+    end
+
+    it 'If a correct template was rendered.' do
+      get :show, params: { user_id: user.id, id: post.id }
+      expect(response).to render_template(:show)
     end
   end
 end
