c75a6697683448ed30368b7ce1e595b87092a32e
cancelei <glauber.bannwart@gmail.com>
2023-10-16 19:58:28 -0300

    Update Seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,23 @@
 # Creating 5 users
 5.times do |i|
   user = User.create!(
-    email: "user#{i + 1}@example.com",
     name: "User #{i + 1}",
     bio: "This is the bio for User #{i + 1}.",
-    photo: "https://example.com/user#{i + 1}.jpg"
+    photo_url: "https://example.com/user#{i + 1}.jpg"
   )
 
   # For each user, creating 3 posts
   3.times do |j|
     post = Post.create!(
       title: "Post #{j + 1} by User #{i + 1}",
-      body: "This is the body of Post #{j + 1} by User #{i + 1}.",
+      text: "This is the body of Post #{j + 1} by User #{i + 1}.",
       author: user
     )
 
     # For each post, creating 2 comments
     2.times do |k|
       Comment.create!(
-        content: "Comment #{k + 1} on Post #{j + 1} by User #{i + 1}",
+        text: "Comment #{k + 1} on Post #{j + 1} by User #{i + 1}",
         user:,
         post:
       )
