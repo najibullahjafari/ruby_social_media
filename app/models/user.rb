48c1d5aaf1e9ee0075199da81f355980273ce0b9
rails <najib2020202020@gmail.com>
2023-10-21 03:38:10 +0430

    Update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  devise :confirmable
   # Methods
   def recent_posts
     posts.order(created_at: :desc).limit(3)
